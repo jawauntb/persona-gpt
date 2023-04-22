719d36068d603802fb8eb7f13f360f0bf9d35473
Jawaun Brown <jawaun.brown95@gmail.com>
2023-04-22 01:23:52 -0400

    Update gptResponder.ts

--- pages/api/gptResponder.ts
+++ pages/api/gptResponder.ts
@@ -2,6 +2,7 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
 
 export async function getResponsesFromPersonas(inputText: string, productIdea: string, personas: string[]) {
+  console.log(`OPENAI_API_KEY: ${process.env.OPENAI_API_KEY}`); // log the value of the OPENAI_API_KEY environment variable
   const key = process.env.OPENAI_API_KEY;
   const chat = new ChatOpenAI({ openAIApiKey: key, temperature: 0.8 });
 
